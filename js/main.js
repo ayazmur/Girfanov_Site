1061a1c49319482b9095a53ede1ad74aa6d72735
ayazmur <ayazikmur@gmail.com>
2024-03-05 21:39:49 +0500

    Изначальное состояние

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('.nav-button');
+const nav = document.querySelector('.nav');
+
+btn.addEventListener('click', ()=>
+{
+    nav.classList.toggle('menu-open');
+})
+
+let maxValue = int.maxValue;
